52e52a7829262b34545712599a1faac350a21a9b
Kollert Slavomír <53907816+Rndoom04@users.noreply.github.com>
2022-11-11 09:08:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+EXAMPLE
+=============
+
+Compatible with PHP >= 7.0.
+
+This is a library-template for writing custom PHP library functions
+
+## Install
+
+For PHP version **`>= 7.0`**:
+
+```
+composer require Rndoom04/example
+```
+
+## How to use it
+
+Firstly init the library by simply "use".
+
+```
+use Rndoom04\example;
+```
+
+Then set load the library and set it how you want.
+```
+$spamdetector = new \Rndoom04\example\example();
+```
---
+++ src/example.php
@@ -0,0 +1,24 @@
+<?php
+    /*
+     * Example library for PHP by Kollert Slavomí­r
+     * version: 1.0
+     * release date: 2.6.2022
+     */
+
+    namespace Rndoom04\example;
+
+    class example {
+        // Example variable
+        private $example = null;
+        
+        // Construct
+        public function __construct() {
+            
+        }
+
+        // Main function
+        public function main() {
+            
+        }
+    }
+?>
